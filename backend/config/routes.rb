89b68229aaa8233ca1567a22e2fcc17b62dbb6dd
guchio33 <aokihikari3@gmail.com>
2022-07-29 17:20:06 +0900

    routeとrestaurantsControllerのファイルの訂正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :restaurant do
+      resources :restaurants do
         resources :foods,only: %i[index]
       end
       resources :line_foods, only: %i[index create]
